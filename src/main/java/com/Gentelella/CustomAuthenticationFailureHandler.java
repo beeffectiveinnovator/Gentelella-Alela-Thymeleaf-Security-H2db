157ee15c766b8aa67fdb4290c58f5e2023bf96b7
AnhLA <37478019+beeffectiveinnovator@users.noreply.github.com>
2019-11-29 08:24:52 +1100

    integrating thymeleaf tags to template

---
+++ src/main/java/com/Gentelella/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,20 @@
+package com.Gentelella;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException e) throws IOException, ServletException {
+		
+		response.sendRedirect("/errors/page_403");
+	}
+}
--- src/main/java/com/Gentelella/SecurityConfiguration.java
+++ src/main/java/com/Gentelella/SecurityConfiguration.java
@@ -6,37 +6,45 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	private AuthenticationSuccessHandler authenticationSuccessHandler;
+	 
+    @Autowired
+    public SecurityConfiguration(AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
 	public void configure(HttpSecurity http) throws Exception{
 		http
 			.authorizeRequests()
 			.antMatchers("/build/**","/css/**","/images/**","/js/**","/src/**","/vendors/**").permitAll()
-			.antMatchers("/index").access("hasRole('ADMIN')")
-			.antMatchers("/index1").access("hasRole('USER1')")
-			.antMatchers("/index2").access("hasRole('USER2')")
+			.antMatchers("/index").hasRole("ADMIN")
+			.antMatchers("/index2").hasAnyRole("USER2","ADMIN")
+			.antMatchers("/index3").hasAnyRole("USER3","ADMIN")
+			.anyRequest().authenticated()
 			.and()
 			.formLogin()
 				.failureUrl("/login?error")
 				.loginPage("/login")
-				.loginProcessingUrl("/login")
-				.usernameParameter("username")
-				.passwordParameter("password")
+				.successHandler(authenticationSuccessHandler)
 			.and()
 			.logout()
 				.logoutUrl("/logout")
-				.logoutSuccessUrl("/login");
+				.logoutSuccessUrl("/login")
+				.permitAll();
 	}
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
 		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}admin").roles("ADMIN")
+			.withUser("user1").password("{noop}user1").roles("ADMIN")
 			.and()
-			.withUser("user1").password("{noop}user1").roles("USER1")
+			.withUser("user3").password("{noop}user3").roles("USER3")
 			.and()
 			.withUser("user2").password("{noop}user2").roles("USER2");
 	}
